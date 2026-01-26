ffc550ed25cbc46752dea854abdb829238de4c15
reni88-dev <renihafizah88@gmail.com>
2026-01-26 13:25:50 +0700

    changed import

--- src/hooks/queries/useActivityLogs.js
+++ src/hooks/queries/useActivityLogs.js
@@ -1,5 +1,5 @@
 import { useQuery, useMutation, useQueryClient } from '@tanstack/react-query';
-import api from '../../lib/api';
+import api from '../../api/axios';
 
 /**
  * Query keys for activity logs
