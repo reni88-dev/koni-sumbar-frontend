fb1fa279ad23a4d799a4c253877359fca3fc844f
reni88-dev <renihafizah88@gmail.com>
2026-02-08 21:46:52 +0700

    edit baseurl

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
+  baseURL: 'https://koni-sumbar-backend-golang.ka2h0x.easypanel.host',
+  // baseURL: import.meta.env.VITE_API_URL || 'http://localhost:8080',
   headers: {
     'Content-Type': 'application/json',
     'Accept': 'application/json',
